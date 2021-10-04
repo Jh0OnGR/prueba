fd39fcca4d4e70ac10b00caa4ff9f2a7fc0e61af
Jh0OnGR <jhong20192001qgmail.com>
2021-10-03 20:41:42 -0500

    nuevo

---
+++ script.js
@@ -0,0 +1,10 @@
+alert("mensaje en la carpeta js")
+var numero= 4444;
+var nemro2=33.3;
+var nomre="jhon";
+var maculino=true;
+/*alert("el nomre"+ nomre+"la edad es"+numero)*/
+var num=prompt("Ingrese el numero 1");
+var num2=prompt("Ingrese el nuner 2")
+var suma= parseInt(num)+parceInt(num2);
+alert("el valor dela suma es " + suma)
